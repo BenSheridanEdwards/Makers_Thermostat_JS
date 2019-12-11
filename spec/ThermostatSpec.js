1f8120853739bcc9c3aa2af173cca532397c0cda
Daniel <demartinezgatell@gmail.com>
2019-12-11 16:19:07 +0000

    adding temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,6 +9,16 @@ describe('Thermostat', function() {
   });
 
    it("should return 20 for the starting temperature", function() {
-      expect(thermostat.temperature).toEqual(20);
+      expect(thermostat.temperature().toEqual(20);
   });
+
+  //   it("should increase the thermostat's temperature", function(){
+  //     thermostat.increase();
+  //     expect(thermostat.temperature().toEqual(21);
+  // });
+
+    // it("should decrease the thermostat's temperature", function(degrees){
+    //   expect(thermostat.decrease(5).toEqual(15));
+    // });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,5 +4,11 @@ function Thermostat() {
   this.temperature = 20;
 }
 Thermostat.prototype.temperature = function() {
-  return this.temperture;
-}; 
+  return this.temperature;
+};
+
+// Thermostat.prototype.increase = function() {
+//   return this.temperature += 1;
+// };
+
+// Thermostat.prototype.decrease = function(degrees) {
